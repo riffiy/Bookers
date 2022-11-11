0a5113b4308319b87429a573c35f2ffeb356a25c
riffiy <117071448+riffiy@users.noreply.github.com>
2022-11-11 08:23:32 +0000

    [add]bookers1.1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,12 @@ class BooksController < ApplicationController
   
   def index
      @book = Book.new
-   
+  end
+  
+  def create 
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/show'
   end
   
   def show

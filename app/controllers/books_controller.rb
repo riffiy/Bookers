c99be0441f27475cae685f91f80dd27b05d13968
riffiy <117071448+riffiy@users.noreply.github.com>
2022-11-12 09:41:59 +0000

    [add]bookers2.1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,46 @@
 class BooksController < ApplicationController
-  def top
-     @book = Book.new
-  end
   
   def index
      @book = Book.new
+     @books = Book.all
   end
   
   def create 
     @book = Book.new(book_params)
-    @book.save
-    redirect_to '/show'
+    if @book.save
+      flash[:success] = "Book was successfully created"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render "index"
+    end
   end
   
   def show
+    @book = Book.find(params[:id])
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully created"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render "edit"
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+    
   
 private
   def book_params
